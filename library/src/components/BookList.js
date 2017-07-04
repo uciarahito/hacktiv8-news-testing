a7b498922b6a7788488432269ae436d0ac58a451
arahito <uarahito@yahoo.com>
2017-07-04 10:41:37 +0700

    add fitur testing for BookList.test.js

--- library/src/components/BookList.js
+++ library/src/components/BookList.js
@@ -1,12 +1,112 @@
 import React from 'react'
 
+import axios from 'axios'
+
 class BookList extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            booklist: []
+        }
+    }
+
+    newStateBooklist(data) {
+        this.setState({
+            booklist: [...this.state.booklist, data]
+        })
+    }
+    
+    addBook() {
+        const newBook = {
+            id: 4,
+            nama_buku: "winter in tokyo",
+            harga: 213000
+        }
+        axios.post('http://localhost:4000/books', newBook)
+        .then(response => {
+            this.newStateBooklist(newBook)
+        })
+        .catch(error => {
+            console.log(`oops, something error when add book: `, error)
+        })
+    }
+
+    removeBook(bookToDeleteId) {
+        this.setState({
+            booklist: this.state.booklist.filter(book => book.id !== bookToDeleteId)
+        })
+    }
+
+    deleteBook(id) {
+        axios.delete('http://localhost:4000/books', id)
+        .then(response => {
+            this.removeBook(id)
+        })
+        .catch(error => {
+            console.log(`oops, something error when delete book: `, error)
+        })
+    }
+
+    editBook() {
+
+    }
+
+    fetchAllBook(data) {
+        this.setState({
+            booklist: data
+        })
+    }
+
+    componentDidMount() {
+        let self = this
+
+        axios.get('http://localhost:4000/books')
+        .then(response => {
+            console.log('data semua buku: ', response.data)
+
+            this.fetchAllBook(response.data)
+        })
+        .catch(error => {
+            console.log(`oops, something error when fetch All book: `, error)
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Ini BookList</h1>
+                <table style={styles.table}>
+                    <thead>
+                        <th>No</th>
+                        <th>Judul Buku</th>
+                        <th>Harga</th>
+                        <th>Operasi</th>
+                    </thead>
+                    <tbody>
+                        {this.state.booklist.map((book, item) => {
+                            return (
+                                <tr key={book.id}>
+                                    <td>{book.id}</td>
+                                    <td>{book.nama_buku}</td>
+                                    <td>{book.harga}</td>
+                                    <td>
+                                        <button onClick={() => this.addBook()}>Add</button>
+                                        <button>Edit</button>
+                                        <button onClick={() => this.deleteBook(book.id)}>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
             </div>
         )
+    }
+}
+
+const styles = {
+    table: {
+        borderStyle: 'solid'
     }
 }
 
---
+++ library/src/components/BookList.test.js
@@ -0,0 +1,98 @@
+import React from 'react'
+
+import { shallow } from 'enzyme'
+
+import BookList from './BookList'
+
+describe('<BookList> component ', () => {
+    
+    it('renders a <div> ', () => {
+        const bookListWrapper = shallow(<BookList />)
+        const divSection = bookListWrapper.find('div')
+        expect(divSection).toHaveLength(1)
+    })
+
+    it('have booklist state which consistes of three items ', () => {
+        const bookListWrapper = shallow(<BookList />)
+        const actualState = [
+            {
+                "id": 1,
+                "nama_buku": "caricari",
+                "harga": "3434983"
+            },
+            {
+                "id": 2,
+                "nama_buku": "Si Juki",
+                "harga": 10000
+            },
+            {
+                "id": 3,
+                "nama_buku": "indonesia",
+                "harga": "900022"
+            }
+        ]
+        bookListWrapper.instance().fetchAllBook(actualState)
+        const booklistState = bookListWrapper.state('booklist')
+        expect(booklistState).toHaveLength(3)
+    })
+
+    it('add a new book to the state when addBook function called ', () => {
+        const bookListWrapper = shallow(<BookList />)
+
+        const actualState = [
+            {
+                "id": 1,
+                "nama_buku": "caricari",
+                "harga": "3434983"
+            },
+            {
+                "id": 2,
+                "nama_buku": "Si Juki",
+                "harga": 10000
+            },
+            {
+                "id": 3,
+                "nama_buku": "indonesia",
+                "harga": "900022"
+            }
+        ]
+        bookListWrapper.instance().fetchAllBook(actualState)
+
+        const newBook = {
+            id: 4,
+            nama_buku: "winter in tokyo",
+            harga: 213000
+        }
+        bookListWrapper.instance().newStateBooklist(newBook)
+        const booklistState = bookListWrapper.state('booklist')
+        expect(booklistState).toHaveLength(4)
+    })
+
+    it('delete a book from a state when removeBook function called', () => {
+        const bookListWrapper = shallow(<BookList />)
+
+        const actualState = [
+            {
+                "id": 1,
+                "nama_buku": "caricari",
+                "harga": "3434983"
+            },
+            {
+                "id": 2,
+                "nama_buku": "Si Juki",
+                "harga": 10000
+            },
+            {
+                "id": 3,
+                "nama_buku": "indonesia",
+                "harga": "900022"
+            }
+        ]
+        bookListWrapper.instance().fetchAllBook(actualState)
+
+        const bookToDeleted = 3
+        bookListWrapper.instance().removeBook(bookToDeleted)
+        const booklistState = bookListWrapper.state('booklist')
+        expect(booklistState).toHaveLength(2)
+    })
+})
